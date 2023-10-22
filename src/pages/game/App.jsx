0d257057d5a201b1c2a85270ff83453e76ce10f7
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-22 18:11:49 -0400

    feat(src): added JoystickCtrl and Fullscreen components in GameView

--- src/pages/game/App.jsx
+++ src/pages/game/App.jsx
@@ -1,12 +1,40 @@
 
+import {useState} from "react"
 import Button from "ui/Button"
+import JoystickCtrl from "./controls/JoystickCtrl"
+import Fullscreen from "ui/Fullscreen"
+import GameRenderer from "ui/GameRenderer"
+
+import spaceImg from "assets/outer-space-6988255_960_720.jpg"
 
 // Application 
 export default function App () {
-    return (
-      <div> 
-        App
-        <Button> Ok </Button>
-      </div>
-    )
+    const [fullscreen, setFullscreen] = useState(false);
+    
+    return (<div>
+        {!fullscreen &&
+            <Button onClick={() => setFullscreen(true)}> Start </Button> 
+        }
+        
+        {fullscreen && 
+            <Fullscreen 
+                fullscreen={fullscreen} 
+                onFullscreen={(full) => setFullscreen(full)}
+            >
+                <div style={{
+                    width: "100%",
+                    height: "100%",
+                    background: "url(" + spaceImg + ")"
+                }}/>  
+                <JoystickCtrl 
+                    style={{
+                        position: "absolute",
+                        bottom: "1rem",
+                        left: "1rem"
+                    }}
+                    onMove={()=>{}}
+                />
+            </Fullscreen>
+        }
+    </div>)
 }
