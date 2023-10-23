4d549de0d2aba91dd63d4a2d4a049a22cf876715
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-23 14:05:02 -0400

    feat(src): added AudioSprite GL-Component

---
+++ src/pages/game/gl/AudioSprite.js
@@ -0,0 +1,13 @@
+
+import { Howl } from "howler"
+
+/**
+ * Howl API wrapper
+ */
+class AudioSprite extends Howl {
+    constructor (object) {
+        super(object);
+    }
+}
+
+export default AudioSprite;
