c23de426b6c063986a32a24e24bd6d5a0ea8071d
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-22 23:32:46 -0400

    feat(src): added basic game loop

---
+++ src/pages/game/engine/loop.js
@@ -0,0 +1,31 @@
+
+import { gx } from "engine/scene";
+
+const PI = Math.PI;
+
+export default function loop () {
+    const {
+        player,
+        camera,
+        joy,
+        debugText,
+    } = gx;
+    
+    if (!gx.scene) return;
+    
+    let joyRotation = Math.atan2(joy.y, joy.x);// + PI;
+    
+    let vx = player.speed * joy.x;
+    let vy = player.speed * joy.y;
+    
+    player.vx += (vx - player.vx) * 0.01;
+    player.vy += (vy - player.vy) * 0.01; 
+    
+    player.x += player.vx;
+    player.y += player.vy;
+    camera.x += (player.x - camera.x) * 0.03;
+    camera.y += (player.y - camera.y) * 0.03; 
+    player.rotation += (joyRotation - player.rotation) * 0.09;
+    
+    //debugText.text = player.rotation / PI + "π"
+}
