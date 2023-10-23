877fa87c7d23ac86124b7b1b5bd9af0fd14e772f
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-23 14:09:52 -0400

    feat(src): added [addSound] method to resource loader

--- src/pages/game/gl/Loader.js
+++ src/pages/game/gl/Loader.js
@@ -1,37 +1,92 @@
 
 import { Texture } from "pixi.js"
 
+/**
+ * Loader and resources preprocessor 
+ */
 class Loader {
     constructor () {
         this.assets = [];
         this.resources = {};
     }
     
-    add ([name, src, preprocess]) {
-        this.assets.push([name, src, preprocess]);
+    /**
+     * Add a texture assets
+     * 
+     * @param {string} name - asset id
+     * @param {string} src - asset url
+     * @param {function} preprocess - texture ready
+     * @return {Loader}
+     */
+    add ({name, src, preprocess}) {
+        this.assets.push({
+            name, 
+            src, 
+            type: "texture",
+            preprocess,
+        });
         return this;
     }
     
+    /**
+     * Add a sound assets
+     * 
+     * @param {string} name - asset id
+     * @param {SoundSprite.params} cfg - configuration 
+     * @param {function} preprocess - sound ready
+     * @return {Loader} 
+     */
+    addSound ({name, cfg, preprocess}) {
+        this.assets.push({
+            name, 
+            src, 
+            type: "sound",
+            preprocess,
+        });
+        return this;
+    }
+    
+    /**
+     * Start load all assets
+     * 
+     * @param {function} callback - assets ready
+     */
     load (callback) {
         const assets = this.assets;
         const resources = this.resources;
         
         let promise = Promise.resolve();
         while (assets.length) {
-            let [name, src, preprocess] = assets.shift();
+            let {name, src, preprocess, type} = assets.shift();
             
             promise = promise.then(() => new Promise((resolve)=>{
-                let texture = Texture.from(src);
-                texture.baseTexture.on("loaded", () => {
-                    texture.baseTexture.removeListener("loaded");
-                    if (preprocess) preprocess(texture);
-                    resources[name] = texture;
-                    resolve();
-                });
+                
+                // texture asset
+                if (type === "texture") { 
+                    let texture = Texture.from(src);
+                    texture.baseTexture.on("loaded", () => {
+                        texture.baseTexture.removeListener("loaded");
+                        if (preprocess) preprocess(texture);
+                        resources[name] = texture;
+                        resolve();
+                    });
+                } 
+               
+                // sound asset
+                else if (type === "sound") {
+                    let sound = new SoundSprite(src);
+                    sound.once("load", () => {
+                        sound.removeListener("load");
+                        if (preprocess) preprocess(sound);
+                        resources[name] = sound;
+                        resolve();
+                    });
+                }
             }));
+            
         }
         
-        promise.then(()=>callback());
+        promise.then(() => callback());
         return this;
     }
 }
