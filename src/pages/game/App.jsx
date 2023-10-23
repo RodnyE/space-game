ff5624adbc658e1efbcb1dd8489a83668912e1e9
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-22 23:33:47 -0400

    feat(src): update game view setup

--- src/pages/game/App.jsx
+++ src/pages/game/App.jsx
@@ -1,15 +1,30 @@
 
-import {useState} from "react"
+import { useState, useEffect } from "react"
+
 import Button from "ui/Button"
 import JoystickCtrl from "./controls/JoystickCtrl"
 import Fullscreen from "ui/Fullscreen"
 import GameRenderer from "ui/GameRenderer"
 
-import spaceImg from "assets/outer-space-6988255_960_720.jpg"
+import { gx, createScene } from "engine/scene"
+import Container from "gl/Container"
+import loopHandler from "engine/loop"
 
 // Application 
 export default function App () {
     const [fullscreen, setFullscreen] = useState(false);
+    const [scene, setScene] = useState(new Container());
+    const [play, setPlay] = useState(null);
+    
+    // Mount GameRenderer
+    const rendererHandler = (renderer) => {
+        if (renderer) {
+            gx.renderer = renderer;
+            gx.joy = {x: 0, y: 0, s: 0}
+            setScene(createScene());
+            setPlay(true);
+        }
+    };
     
     return (<div>
         {!fullscreen &&
@@ -21,18 +36,21 @@ export default function App () {
                 fullscreen={fullscreen} 
                 onFullscreen={(full) => setFullscreen(full)}
             >
-                <div style={{
-                    width: "100%",
-                    height: "100%",
-                    background: "url(" + spaceImg + ")"
-                }}/>  
+                <GameRenderer 
+                    scene={scene} 
+                    height={500}
+                    ratio={3/2}
+                    play={play}
+                    onLoop={loopHandler}
+                    onRenderer={rendererHandler}
+                />  
                 <JoystickCtrl 
                     style={{
                         position: "absolute",
                         bottom: "1rem",
                         left: "1rem"
                     }}
-                    onMove={()=>{}}
+                    onMove={(joy) => {gx.joy = joy}}
                 />
             </Fullscreen>
         }
