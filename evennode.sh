30d2e3849c81bd756cc45b63b0b9e0cfa2718b42
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-10-24 11:43:48 -0400

    chore: added evennode.sh upload script bash

---
+++ evennode.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# Bash script to upload production repo to evennode
+
+BCyan='\033[1;36m'
+EndColor='\033[0m'
+
+# compile 
+echo -e "\n${BCyan}Compiling...${EndColor}\n"
+npm run build
+git add dist
+
+# create new database
+echo -e "\n${BCyan}Creating database...${EndColor}\n"
+npm run inst 5 5
+git add database -f
+
+# clear unnecessary files
+git rm --cached src 
+git rm --cached public
+
+# push
+git commit -m "[[update evennode]]"
+echo -e "\n${BCyan}Uploading to evennode...${EndColor}\n"
+git push evennode main -f
+
+# restore
+git reset --soft HEAD~1
+git restore --staged database src public 
+
+echo -e "\n${BCyan}Success !!${EndColor}\n"
