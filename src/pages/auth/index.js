f836a0da056bc225fe2d1767a5dc65e89bfff1ac
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-11-07 02:18:39 -0500

    chore (src/auth): added full bootstrap and eruda module in AuthPage

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import AuthPage from './App'
-import 'bootstrap/dist/css/bootstrap-utilities.css'
 import 'styles/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
+import eruda from 'eruda'
+eruda.init()
 
 // render
 const root = createRoot(document.getElementById('root'));
