28248fc405fd1e81d06da0b28f1036289fbbd58a
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-11-07 02:24:05 -0500

    feat (src/auth): added local login on AuthPage

--- src/pages/auth/App.jsx
+++ src/pages/auth/App.jsx
@@ -1,11 +1,58 @@
 
+import { useState } from "react"
+import http from "utils/http"
+
 import AuthButton from "ui/AuthButton"
+import TextField from "ui/TextField"
+import Button from "ui/Button"
 
 export default function App () {
+    const [emailField, setEmailField] = useState("");
+    const [passField, setPassField] = useState("");
+    
+    /**
+     * Send login data to server
+     */
+    const sendLogin = () => {
+        http.post({
+            url: "/auth/login",
+            body: {
+                username: emailField,
+                password: passField,
+            }
+        })
+        .then(() => {
+            
+        })
+    }
+    
     return (
         <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+            
+            <div className="m-5">
+                <TextField 
+                    type="text" 
+                    placeholder="Usuario:"
+                    value={emailField}
+                    onInput={(e) => setEmailField(e.target.value)}
+                />
+                <TextField 
+                    type="password" 
+                    placeholder="Contraseña:"
+                    value={passField}
+                    onInput={(e) => setPassField(e.target.value)}
+                />
+                <AuthButton 
+                    className="d-flex p-2 justify-content-center"
+                    onClick={sendLogin}
+                > 
+                    Acceder 
+                </AuthButton>
+            </div> 
+            
             <AuthButton type="google"/>
             <AuthButton type="facebook"/>
+            
         </div>
     )
 }
