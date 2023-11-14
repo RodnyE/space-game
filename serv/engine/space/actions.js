636b4566336f23e4e8a67ae5eb6dfb5cf6df5d5a
TheNewestNewbie <thenewestnewbie@gmail.com>
2023-11-14 13:14:01 -0500

    feat (serv/colonization): added colonization features

--- serv/engine/space/actions.js
+++ serv/engine/space/actions.js
@@ -1,5 +1,6 @@
 const config = require("../../../config.js");
 const uid = require(config.HELPERS + "/uid.js");
+const {Rand} = require(config.HELPERS + "/maths.js");
 const { SpaceZone, Planet, Op } = require(config.HELPERS + "/db.js");
 
 let items = {};
@@ -11,35 +12,42 @@ const setItems = (_items) => {
 class Action {
     constructor(id) {
         this.id = id;
-        this.types = ["TERRAFORM", "COLONIZATION"]
+        this.types = ["TERRAFORM", "COLONIZATION"];
     }
 
     typeOfAction() {
         switch (true) {
             case this.id.includes("tm"):
-
                 return this.types[0];
 
             case this.id.includes("cm"):
-
                 return this.types[1];
 
             default:
-
+            // handle unexpected case
         }
     }
-
 
     async executeItem(player, g) {
         const space_pos = player.space_pos;
         const pos = player.pos;
-        if(!player.inventory.hasItem(this.id)) return player.Emit("toast", { status: false, message: "ITEM_NOT_FOUND" });
+
+        if (!player.inventory.hasItem(this.id)) {
+            return player.Emit("toast", { status: false, message: "ITEM_NOT_FOUND" });
+        }
+        let space , planets , found;
         switch (this.typeOfAction()) {
-            case this.types[0]:
-                const space = await SpaceZone.findOne({ where: { x: space_pos.x, y: space_pos.y } });
-                if (!space) return player.Emit("toast", { status: false, message: "SPACE_NOT_FOUND" });
-                const planets = Planet.findAll({ where: { spacezone_id: space.id } });
-                const found = null;
+            case this.types[0]: // TERRAFORM action
+                space = await SpaceZone.findOne({ where: { x: space_pos.x, y: space_pos.y } });
+                if (!space) {
+                    console.error("Space not found");
+                    return player.Emit("toast", { status: false, message: "SPACE_NOT_FOUND" });
+                }
+
+                planets = await Planet.findAll({ where: { spacezone_id: space.id } });
+                found = null;
+
+                // Iterate through planets to find the one at player's position
                 for (let planet of planets) {
                     const radius = planet.diameter / 2;
                     const min_x = planet.x - radius,
@@ -52,41 +60,186 @@ class Action {
                         break;
                     }
                 }
-                if (!found) return player.Emit("toast", { status: false, message: "PLANET_NOT_FOUND" });
-                if (found.owner) return player.Emit("toast", { status: false, message: "PLANET_ALREADY_COLONIZED" });
+
+                // Check if a planet is found at the player's position
+                if (!found) {
+                    return player.Emit("toast", { status: false, message: "PLANET_NOT_FOUND" });
+                }
+
+                // Check if the planet is already terraforming
+                if (found.isTerraforming) {
+                    return player.Emit("toast", { status: false, message: "PLANET_ALREADY_TERRAFORMING" });
+                }
+
+                // Check if the planet is already colonized
+                if (found.owner) {
+                    return player.Emit("toast", { status: false, message: "PLANET_ALREADY_COLONIZED" });
+                }
+
+                // Check if the required item exists
                 if (items[this.id]) {
                     const item = items[this.id];
-                    if (found.temperature > item.temperature) return player.Emit("toast", { status: false, message: "INSUFICIENT_LEVEL_OF_TERRAFORMER" });
-                    if (item.require.energy > player.stats.energy) return player.Emit("toast", { status: false, message: "INSUFICIENT_ENERGY" });
-                    
+
+                    // Check terraforming conditions
+                    if (found.temperature > item.temperature) {
+                        return player.Emit("toast", { status: false, message: "INSUFFICIENT_LEVEL_OF_TERRAFORMER" });
+                    }
+                    if (item.require.energy > player.stats.energy) {
+                        return player.Emit("toast", { status: false, message: "INSUFFICIENT_ENERGY" });
+                    }
+
+                    // Update player stats and inventory
                     player.stats.energy -= item.require.energy;
-                    player.inventory.deleteItem(this.id , 1);
-                    player.Emit("pj_change" , {stats: player.stats});
-                    
-                    const terraformDuration = 60; 
+                    player.inventory.deleteItem(this.id, 1);
+                    player.Emit("pj_change", { stats: player.stats });
+
+                    const terraformDuration = 60;
                     const tickInterval = 1000;
 
                     let remainingTime = terraformDuration;
-                    g.BroadcastToRoom(space_pos.x + "_" + space_pos,y , "space_change" , {name: found.name , isTerraforming: 60});
+
+                    // Broadcast terraforming progress to the room
+                    g.BroadcastToRoom(space_pos.x + "_" + space_pos.y, "space_change", { name: found.name, isTerraforming: 60 });
+
+                    // Set planet to terraforming
+                    found.isTerraforming = remainingTime;
+                    await found.save();
+
+                    // Execute terraforming with a timer
                     const tick = setInterval(async () => {
                         remainingTime--;
-               
+
                         found.isTerraforming = remainingTime;
 
                         if (remainingTime <= 0) {
                             clearInterval(tick);
-                            found.isTerraforming = false;
-                            found.owner = player.id; 
-                            g.BroadcastToRoom(space_pos.x + "_" + space_pos,y , "space_change" , {name: found.name , isTerraforming: 0});
-                            await found.save(); 
+                            found.isTerraforming = 0; // Set terraforming to 0 instead of false
+                            found.temperature = Rand(-30 , 30);
+
+                            // Broadcast terraforming completion to the room
+                            g.BroadcastToRoom(space_pos.x + "_" + space_pos.y, "space_change", { name: found.name, isTerraforming: 0 });
+
+                            // Save the updated planet information
+                            await found.save();
+
+                            // Inform the player about terraforming completion
                             player.Emit("toast", { status: true, message: "TERRAFORMATION_COMPLETED" });
                         }
                     }, tickInterval);
-                } else return player.Emit("toast", { status: false, message: "ITEM_NOT_FOUND" });
+                } else {
+                    // Item not found in items database
+                    return player.Emit("toast", { status: false, message: "ITEM_NOT_FOUND" });
+                }
+                break;
+
+            case this.types[1]: // COLONIZATION action
+                space = await SpaceZone.findOne({ where: { x: space_pos.x, y: space_pos.y } });
+                if (!space) {
+                    console.error("Space not found");
+                    return player.Emit("toast", { status: false, message: "SPACE_NOT_FOUND" });
+                }
+
+                planets = await Planet.findAll({ where: { spacezone_id: space.id } });
+                found = null;
+
+                // Iterate through planets to find the one at player's position
+                for (let planet of planets) {
+                    const radius = planet.diameter / 2;
+                    const min_x = planet.x - radius,
+                        max_x = planet.x + radius,
+                        min_y = planet.y - radius,
+                        max_y = planet.y + radius;
+
+                    if (pos.x > min_x && pos.x < max_x && pos.y > min_y && pos.y < max_y) {
+                        found = planet;
+                        break;
+                    }
+                }
+
+                // Check if a planet is found at the player's position
+                if (!found) {
+                    return player.Emit("toast", { status: false, message: "PLANET_NOT_FOUND" });
+                }
+
+                // Check if the planet is already colonized
+                if (found.owner) {
+                    return player.Emit("toast", { status: false, message: "PLANET_ALREADY_COLONIZED" });
+                }
+
+                // Check if the planet is already in the process of terraforming
+                if (found.isTerraforming != 0) {
+                    return player.Emit("toast", { status: false, message: "PLANET_TERRAFORMING" });
+                }
+
+                // Check if the planet is already in the process of colonizing
+                if (found.isColonizing != 0) {
+                    return player.Emit("toast", { status: false, message: "PLANET_COLONIZING" });
+                }
+
+                // Check if the temperature of the planet is in the suitable range for colonization
+                if (found.temperature < -30 || found.temperature > 30) {
+                    return player.Emit("toast", { status: false, message: "UNSUITABLE_TEMPERATURE_FOR_COLONIZATION" });
+                }
+
+                // Check if the required item exists
+                if (items[this.id]) {
+                    const item = items[this.id];
+
+                    // Check colonization conditions
+                    if (item.require.energy > player.stats.energy) {
+                        return player.Emit("toast", { status: false, message: "INSUFFICIENT_ENERGY" });
+                    }
+
+                    // Update player stats and inventory
+                    player.stats.energy -= item.require.energy;
+                    player.inventory.deleteItem(this.id, 1);
+                    player.Emit("pj_change", { stats: player.stats });
+
+                    // Set the duration and time interval for colonization
+                    const colonizationDuration = 60;
+                    const tickInterval = 1000;
+
+                    let remainingTime = colonizationDuration;
+
+                    // Broadcast colonization progress to the room
+                    g.BroadcastToRoom(space_pos.x + "_" + space_pos.y, "space_change", { name: found.name, isColonizing: 60 });
+
+                    // Set the planet to the colonizing state
+                    found.isColonizing = remainingTime;
+                    await found.save();
+
+                    // Execute colonization with a timer
+                    const tick = setInterval(async () => {
+                        remainingTime--;
+
+                        found.isColonizing = remainingTime;
+
+                        if (remainingTime <= 0) {
+                            clearInterval(tick);
+                            found.isColonizing = 0; // Set colonization to 0 
+                            found.owner = player.id;
+
+                            // Broadcast colonization completion to the room
+                            g.BroadcastToRoom(space_pos.x + "_" + space_pos.y, "space_change", { name: found.name, isColonizing: 0 });
+
+                            // Save the updated planet information
+                            await found.save();
+
+                            // Inform the player about colonization completion
+                            player.Emit("toast", { status: true, message: "COLONIZATION_COMPLETED" });
+                        }
+                    }, tickInterval);
+                } else {
+                    // Item not found in items database
+                    return player.Emit("toast", { status: false, message: "ITEM_NOT_FOUND" });
+                }
+                break;
+
+            default:
+                // handle unexpected case
                 break;
         }
     }
 }
-
 
 module.exports = { setItems, Action };
--- serv/engine/space/space.js
+++ serv/engine/space/space.js
@@ -2,8 +2,8 @@ const config = require("../../../config.js");
 const nameGen = require("../generation/nameGen.js");
 const { Player, SpaceZone, Planet, Op } = require(config.HELPERS + "/db.js");
 const Maths = require(config.HELPERS + "/maths.js");
-const {setItems}  = require("../player/inventory.js");
-const actions  = require("./actions.js");
+const { setItems } = require("../player/inventory.js");
+const actions = require("./actions.js");
 const Action = actions.Action;
 
 class _Space {
@@ -17,43 +17,58 @@ class _Space {
     }
 
     async config() {
+        // Calculate the size of the space grid based on the number of saved spaces
         const c = Math.ceil(Math.sqrt(await SpaceZone.count() - config.SPACES.saved));
+
+        // Initialize default spaces and player changes for default spaces
         this.space[-1 + "_" + -1] = {};
         this.pj_changes[-1 + "_" + -1] = {};
         this.space[-1 + "_" + 0] = {};
         this.pj_changes[-1 + "_" + 0] = {};
         this.space[0 + "_" + -1] = {};
         this.pj_changes[0 + "_" + -1] = {};
+
+        // Initialize spaces and player changes for the dynamic space grid
         for (let x = 0; x < c; x++) {
             for (let y = 0; y < c; y++) {
                 this.space[x + "_" + y] = {};
                 this.pj_changes[x + "_" + y] = {};
             }
         }
+
+        // Load items and set them in the inventory
         this.items = require("./items.js")();
         setItems(this.items);
         actions.setItems(this.items);
+
+        // Load buildings
         this.buildings = require("./buildings.js")();
     }
 
     async addPlayer(player) {
+        // Add a player to the space
         this.players[player.id] = player;
         let space_pos = player.space_pos;
         let space = space_pos.x + "_" + space_pos.y;
 
+        // Function to handle space changes when a player moves to an edge
         const changeSpaces = async (data) => {
+            // Check if the player is at the edge of the space grid
             if (data.x < 0 || data.x >= 1000 || data.y < 0 || data.y >= 1000) {
+                // Function to polarize coordinates
                 const polarize = (data) => {
                     let mx = (data.x < 0 ? 999 : (data.x >= 1000 ? 1 : data.x));
                     let my = (data.y < 0 ? 999 : (data.y >= 1000 ? 1 : data.y));
                     return { x: mx, y: my };
                 };
-    
+
+                // Function to mirror coordinates
                 const mirror = async (x) => {
                     const c = Math.floor(Math.sqrt((await SpaceZone.count()) - config.SPACES.saved));
                     return (x >= c ? 0 : (x < 0 ? c : x));
                 }
 
+                // Handle the case when the player is at the starting point (-1, -1)
                 if (space_pos.x == -1 && space_pos.y == -1) {
                     const sp = await SpaceZone.findAll({
                         where: {
@@ -63,6 +78,7 @@ class _Space {
                     let c = sp.length;
                     let found = false;
 
+                    // Find a suitable sun space for colonization
                     while (found == false) {
                         const s = sp[Maths.Rand(0, c - config.SPACES.saved)];
 
@@ -73,21 +89,24 @@ class _Space {
                                 owner: null,
                             }
                         });
+
                         if (planets.length > 0) {
                             found = s;
                             break;
                         }
-
                     }
+
+                    // Polarize the coordinates and change the player's space
                     const p = polarize(data);
-                    player.inventory.addItem("cm1" , 1 , true);
-                    player.inventory.addItem("tm3" , 1 , true);
-                    player.shortcuts.addShortcut("cm1" , 0);
-                    player.shortcuts.addShortcut("tm3" , 1);
+                    player.inventory.addItem("cm1", 1, true);
+                    player.inventory.addItem("tm3", 1, true);
+                    player.shortcuts.addShortcut("cm1", 0);
+                    player.shortcuts.addShortcut("tm3", 1);
                     await player.changeSpace(p, found, this.space);
 
                     return true;
                 } else {
+                    // Handle other cases when the player is at an edge
                     let mx = (data.x < 0 ? -1 : (data.x >= 1000 ? 1 : 0));
                     let my = (data.y < 0 ? -1 : (data.y >= 1000 ? 1 : 0));
                     let nx = await mirror(player.space_pos.x + mx);
@@ -106,6 +125,7 @@ class _Space {
             }
         };
 
+        // Set up player movement handling
         player.setEnableMove(() => {
             player.On("move", async (data) => {
                 space_pos = player.space_pos;
@@ -128,17 +148,20 @@ class _Space {
             });
         });
 
+        // Join the player to the space and enable movement
         await player.joinSpace(space_pos.x, space_pos.y, this.space);
         player.enableMove();
         player.nameChange(this.space);
 
-        player.On("shortcut" , async(data) => {
+        // Handle player shortcuts
+        player.On("shortcut", async (data) => {
             const item = player.shortcuts.getShortcut(data);
-            if(!item) return;
+            if (!item) return;
             const action = new Action(item);
-            await action.executeItem(player , this.g);
+            await action.executeItem(player, this.g);
         });
 
+        // Handle player disconnection
         player.On("disconnect", async (data) => {
             player.leaveSpace(this.space);
             const p = await Player.findOne({ where: { user_id: player.id } });
@@ -151,34 +174,30 @@ class _Space {
             space_pos = player.space_pos;
             space = space_pos.x + "_" + space_pos.y;
             delete this.players[player.id];
-
         });
-
     }
 
-
     Loop(fps) {
-
+        // Function to check if an object is empty
         const isEmptyObject = (obj) => {
             return Object.values(obj).length === 0;
         }
-        //Game Loop
+
+        // Game Loop
         setInterval(() => {
             const pj_changes = { ...this.pj_changes };
             for (let m in pj_changes) {
                 if (isEmptyObject(pj_changes[m])) continue;
-                this.g.BroadcastToRoom(m, 'pj_changes', pj_changes[m]); //sending players pj_changes to area
-                this.pj_changes[m] = {}
+                this.g.BroadcastToRoom(m, 'pj_changes', pj_changes[m]); // sending players pj_changes to area
+                this.pj_changes[m] = {};
             }
-            //this.pj_changes = {}; //restart the var
+            // this.pj_changes = {}; // restart the variable
         }, 1000 / fps || 30);
     }
 
     async ResourcesLoop(time) {
-
+        // To be implemented
     }
-
-
 }
 
 module.exports = _Space;
